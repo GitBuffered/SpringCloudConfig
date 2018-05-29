f5777640e793d520c49cf760dc368b488c6b7c91
chixue <334548450@qq.com>
2018-05-29 18:04:09 +0800

    提交CONFIG 自动、手动刷新的代码，详细的资料可查看该工程的WIKI

---
+++ springcloud-config-client-refresh-bus/src/main/java/org/springcloud/config/client/refresh/bus/ConfigClientController.java
@@ -0,0 +1,18 @@
+package org.springcloud.config.client.refresh.bus;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+	@Value("${test}")
+	private String profile;
+	    
+	@RequestMapping("/test")
+    public String home() {
+        return this.profile;
+    }
+}
